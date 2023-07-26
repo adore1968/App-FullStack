d751492790cf39466a1b0321eb8b4f817f10a03b
German <101434158+adore1968@users.noreply.github.com>
2023-07-26 15:54:43 -0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://mern-api.onrender.com/api",
+  baseURL: "https://mern-api-q29z.onrender.com/api",
   withCredentials: true,
 });
 
